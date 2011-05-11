4aab816289c4e9bcb0379f81ec289a6db952c5aa
John Lusk <john-training@how-hard-can-it-be.com>
2011-05-11 17:51:25 -0400

    Pushing new changes

--- source/nothinbutdotnetstore/web/core/CommandRegistry.cs
+++ source/nothinbutdotnetstore/web/core/CommandRegistry.cs
@@ -1,12 +1,21 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace nothinbutdotnetstore.web.core
 {
     public class CommandRegistry : IFindSpecificRequestHandlers
     {
+        IEnumerable<IProcessOneUniqueRequest> handlers;
+
+        public CommandRegistry(IEnumerable<IProcessOneUniqueRequest> handlers)
+        {
+            this.handlers = handlers;
+        }
+
         public IProcessOneUniqueRequest get_the_command_that_can_process(IContainRequestInformation request)
         {
-            throw new NotImplementedException();
+            return handlers.First(x => x.can_handle(request));
         }
     }
 }
