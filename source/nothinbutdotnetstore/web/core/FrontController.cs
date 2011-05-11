facae66c4e9f77829b066456809bad6c60b2f86a
Your Name <you@email.com>
2011-05-11 17:35:21 -0400

    Pushing new changes

--- source/nothinbutdotnetstore/web/core/FrontController.cs
+++ source/nothinbutdotnetstore/web/core/FrontController.cs
@@ -4,9 +4,17 @@ namespace nothinbutdotnetstore.web.core
 {
     public class FrontController : IProcessApplicationRequests
     {
+        IFindSpecificRequestHandlers requestHandler;
+
+        public FrontController(IFindSpecificRequestHandlers request_handler)
+        {
+            requestHandler = request_handler;
+        }
+
         public void process(IContainRequestInformation request)
         {
-            throw new NotImplementedException();
+            requestHandler.get_the_command_that_can_process(request).run(request);
         }
+
     }
 }
