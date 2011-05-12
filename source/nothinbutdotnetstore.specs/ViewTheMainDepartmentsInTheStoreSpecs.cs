ab30e2716a499138913945632097a95c62155efd
John Lusk <john-training@how-hard-can-it-be.com>
2011-05-12 14:01:34 -0400

    Pushing new changes

--- source/nothinbutdotnetstore.specs/ViewTheMainDepartmentsInTheStoreSpecs.cs
+++ source/nothinbutdotnetstore.specs/ViewTheMainDepartmentsInTheStoreSpecs.cs
@@ -2,6 +2,7 @@ using developwithpassion.specifications.rhinomocks;
 using Machine.Specifications;
 using nothinbutdotnetstore.web.application.catalogbrowsing;
 using nothinbutdotnetstore.web.core;
+using developwithpassion.specifications.extensions;
 
 namespace nothinbutdotnetstore.specs
 {
@@ -18,15 +19,42 @@ namespace nothinbutdotnetstore.specs
       Establish c = () =>
       {
         request = fake.an<IContainRequestInformation>();
+
+          list_of_main_depts = depends.on<IListOfMainDepts>();
+          displayer = depends.on<IDisplayData>();
       };
 
       Because b = () =>
         sut.run(request);
 
 
-      It should_blah = () => { };
+      It should_find_the_main_depts_for_the_store = () => list_of_main_depts.received(x => x.Load()) ;       
+
+                                        /* find the main depts
+                                         * display the main depts
+                                         * provide access to the list of main depts
+                                         * return a list of main depts
+                                         * prepare a list of main depts and forward to a view of the list
+                                         */
+
+      It should_display_the_main_depts_for_the_store = () => /* "for_the_store" repeating ourselves? */
+      {
+          displayer.received(x => x.Display(list_of_main_depts));
+      }; 
 
       static IContainRequestInformation request;
+        static IListOfMainDepts list_of_main_depts;
+        static IDisplayData displayer;
     }
   }
+
+    public interface IDisplayData
+    {
+        void Display(IListOfMainDepts list_of_main_depts);
+    }
+
+    public interface IListOfMainDepts
+    {
+        void Load();
+    }
 }
